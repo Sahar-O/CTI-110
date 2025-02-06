61b0fb4b29a129ee6cb7bf9632a64af594f69913
Sahar-O <sh672675@gmail.com>
2025-02-06 10:17:39 -0500

    Add files via upload

---
+++ P1LAB1_OlayaSahar.py
@@ -0,0 +1,11 @@
+# Sahar Olaya
+# 2/4/2025
+# P1LAB1
+# Using the input and print functions to display a name
+
+#Get user input for first name
+first_name = input ("Enter your first name : ")
+last_name = input ("Enter your last name : ")
+
+#Display welcome message using first and last name
+print ("Hello," , first_name, last_name + "!", "Welcome to CTI-110")
