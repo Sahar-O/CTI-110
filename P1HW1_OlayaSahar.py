9fa2631d24d0c245102948809119e3f5ce20c50c
Sahar-O <sh672675@gmail.com>
2025-02-11 11:43:53 -0500

    Add files via upload

---
+++ P1HW1_OlayaSahar.py
@@ -0,0 +1,33 @@
+# Sahar Olaya
+# Feburary 11, 2025
+# P1HW1 
+# A code that collects information from user and displays results
+
+print("-----Calculating Exponent-----")
+print()
+print()
+# Calculatinf Exponents
+# Get data from user
+The_base_num = int(input("Enter an integer as the base value  : " ))
+The_exponent_num = int(input("Enter an integer as the exponent : ")) 
+results = The_base_num ** The_exponent_num 
+print()
+print()
+
+print ( The_base_num,"raised to the power of" ,The_exponent_num, "is" ,results, "!!")
+
+print()
+print()
+
+print("-----Addition and Subtraction-----")
+print()
+print()
+# Addition and Subtraction
+Starting_integer = int(input("Enter a starting integer:"))
+Integer_to_add = int(input("Enter an integer to add:"))
+Integer_to_subtract = int(input("Enter an integer to subtract:"))
+results = Starting_integer + Integer_to_add - Integer_to_subtract
+
+print( Starting_integer,"+" ,Integer_to_add, "-" ,Integer_to_subtract, "is equal to" ,results )
+                      
+                      
